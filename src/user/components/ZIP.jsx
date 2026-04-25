109817a3f7744f6c1515eeecd02fad090c5bb745
TaheeraTabassum <tahiratabassumfarha@gmail.com>
2026-04-25 18:39:27 +0600

    generating pdf

---
+++ src/user/components/ZIP.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ZIP() {
+  return (
+    <div className='text-gray-600 p-2 border w-50 mt-4 rounded-md border-blue-200'>
+     ask to generate a ZIP
+    </div>
+  )
+}
